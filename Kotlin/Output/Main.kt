01bc959e5bcc3d774a6d0fd800abda6bd0328977
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-03 05:00:28 +0000

    Output

---
+++ Kotlin/Output/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+  println("Hello World!")
+  println("I am learning Kotlin.")
+  println("It is awesome!")
+  
+  println(3 + 3)
+  
+  print("Hello World! ")
+  print("I am learning Kotlin. ")
+  print("It is awesome!")
+}
