0bbcee9948a39e660ea7c216f9551768aebdd6e9
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-03 13:56:18 +0000

    Data Types

---
+++ Kotlin/DataTypes/Main.kt
@@ -0,0 +1,48 @@
+fun main(){
+    val myNum: Int = 5                // Int
+    val myDoubleNum: Double = 5.99    // Double
+    val myLetter: Char = 'D'          // Char
+    val myBoolean: Boolean = true     // Boolean
+    val myText: String = "Hello"      // String
+    
+    val myNum1: Byte = 100
+    println(myNum1)
+    
+    val myNum2: Short = 5000
+    println(myNum2)
+    
+    val myNum3: Int = 100000
+    println(myNum3)
+    
+    val myNum4: Long = 15000000000L
+    println(myNum4)
+    
+    val myNum5 = 2147483647  // Int
+    val myNum6 = 2147483648  // Long
+
+    val myNum7: Float = 5.75F
+    println(myNum7)
+
+    val myNum8: Double = 19.99
+    println(myNum8)
+
+    val myNum9: Float = 35E3F
+    val myNum10: Double = 12E4
+    println(myNum9)
+    println(myNum10)
+    
+    val isKotlinFun: Boolean = true
+    val isFishTasty: Boolean = false
+    println(isKotlinFun)   // Outputs true
+    println(isFishTasty)   // Outputs false 
+    
+    val myGrade: Char = 'B'
+    println(myGrade)
+    
+    val myText2: String = "Hello World"
+    println(myText2)
+    
+    val x: Int = 5
+    val y: Long = x.toLong()
+    println(y)
+}
