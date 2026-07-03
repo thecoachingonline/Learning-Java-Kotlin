77a1bc6f9bc27105395308d643f1d006baf3de1b
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-03 05:24:03 +0000

    Variables

---
+++ Kotlin/Variables/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+    var name = "John"
+    val birthyear = 1975
+    
+    println(name)          // Print the value of name
+    println(birthyear)     // Print the value of birthyear
+    
+    var name2: String = "Johny" // String
+    val birthyear2: Int = 1985 // Int
+    
+    println(name2)
+    println(birthyear2)
+    
+    var name3 = "John"
+    name3 = "Robert"
+    println(name3)
+    
+    val pi = 3.14159265359
+    println(pi)
+    
+    val name4 = "John"
+    println("Hello " + name4)
+    
+    val firstName = "John "
+    val lastName = "Doe"
+    val fullName = firstName + lastName
+    println(fullName)
+    
+    val x = 5
+    val y = 6
+    println(x + y) // Print the value of x + y 
+}
