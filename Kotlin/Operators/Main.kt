0144b4f2ea1621973d8e8347ec7c358226a4e1df
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-03 14:04:19 +0000

    Operators

---
+++ Kotlin/Operators/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var x = 100 + 50
+    println(x)
+    
+    var sum1 = 100 + 50       // 150 (100 + 50)
+    var sum2 = sum1 + 250     // 400 (150 + 250)
+    var sum3 = sum2 + sum2    // 800 (400 + 400)
+    
+    println(sum1)
+    println(sum2)
+    println(sum3)
+    
+    var x1 = 10
+    x1 += 5
+    println(x1)
+}
