a469f5a8bd233efc4b8b2bfaad882681805e7180
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-04 05:12:40 +0000

    Booleans

---
+++ Kotlin/Booleans/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val isKotlinFun: Boolean = true
+    val isFishTasty: Boolean = false
+    println(isKotlinFun)   // Outputs true
+    println(isFishTasty)   // Outputs false
+    
+    val x = 10
+    val y = 9
+    println(x > y) // Returns true, because 10 is greater than 9
+    
+    println(10 > 9) // Returns true, because 10 is greater than 9
+    
+    val x1 = 10;
+    println(x1 == 10); // Returns true, because the value of x is equal to 10
+    
+    println(10 == 15); // Returns false, because 10 is not equal to 15
+}
