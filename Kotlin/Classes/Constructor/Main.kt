2499fdc035f321cc10e0f17dfe5ac8f55839e656
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-05 08:01:44 +0000

    Constructor

---
+++ Kotlin/Classes/Constructor/Main.kt
@@ -0,0 +1,28 @@
+class Car {
+  var brand = ""
+  var model = ""
+  var year = 0
+}
+
+fun main() {
+  val c1 = Car()
+  c1.brand = "Ford"
+  c1.model = "Mustang"
+  c1.year = 1969
+  
+  println(c1.brand)
+  println(c1.model)
+  println(c1.year)
+}
+
+class Car(var brand: String, var model: String, var year: Int)
+
+fun main() {
+  val c1 = Car("Ford", "Mustang", 1969)
+  val c2 = Car("BMW", "X5", 1999)
+  val c3 = Car("Tesla", "Model S", 2020)
+
+  println(c1.brand)
+  println(c2.brand)
+  println(c3.brand)
+}
