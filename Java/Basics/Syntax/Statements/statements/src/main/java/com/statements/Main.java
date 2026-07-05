ba0dd6316b776bcc7dead7f52f91da9e855d4687
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-05 08:39:29 +0000

    Statements

---
+++ Java/Basics/Syntax/Statements/statements/src/main/java/com/statements/Main.java
@@ -0,0 +1,9 @@
+package com.statements;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🌏 Hello world!");
+        System.out.println("🇹🇭 Have a good day!");
+        System.out.println("😁 Learning Java is fun!");
+    }
+}
