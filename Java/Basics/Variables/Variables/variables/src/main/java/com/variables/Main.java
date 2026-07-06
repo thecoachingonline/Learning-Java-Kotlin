390d4e7dc1b6e85057d0d8a11372d8784127202f
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-06 02:21:03 +0000

    Variables

---
+++ Java/Basics/Variables/Variables/variables/src/main/java/com/variables/Main.java
@@ -0,0 +1,24 @@
+package com.variables;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+        int myNum2 = 15;
+        myNum2 = 20;  // myNum is now 20
+        System.out.println(myNum2);
+        int myNum3 = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+        System.out.println(myNum3);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
