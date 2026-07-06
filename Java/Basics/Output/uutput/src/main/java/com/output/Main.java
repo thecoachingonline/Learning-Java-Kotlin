4abcb3d4e1ad0ffe51f1a56291e37919797d3b1a
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-06 02:01:07 +0000

    Output

---
+++ Java/Basics/Output/uutput/src/main/java/com/output/Main.java
@@ -0,0 +1,10 @@
+package com.output;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This sentence will work!");
+        //System.out.println(This sentence will produce an error);
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+    }
+}
