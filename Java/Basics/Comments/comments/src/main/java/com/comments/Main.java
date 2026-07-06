30102c9d662554018aa7d037578d9a104b53d284
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-06 02:11:12 +0000

    Comments

---
+++ Java/Basics/Comments/comments/src/main/java/com/comments/Main.java
@@ -0,0 +1,12 @@
+package com.comments;
+
+public class Main {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.print("Hello world!");
+        System.out.println("Hello World"); // This is a comment
+        /* The code below will print the words Hello World
+        to the screen, and it is amazing */
+        System.out.println("Hello World");
+    }
+}
