8d0929cfe160df130933e77be39c3e7399409249
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-12 06:27:26 +0000

    Multiple  Variables

---
+++ Java/Basics/Variables/Multiple/multiple/src/main/java/com/multiple/Main.java
@@ -0,0 +1,12 @@
+package com.multiple;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z); // 61
+
+        int ix, iy, iz;
+        ix = iy = iz = 50;
+        System.out.println(ix + iy + iz); // 150
+    }
+}
