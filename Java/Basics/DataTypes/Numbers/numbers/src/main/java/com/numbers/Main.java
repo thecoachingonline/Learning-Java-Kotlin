b53c15f438d1f95537f46ed5c44c30c2a5bbfd80
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-14 02:26:26 +0000

    Numbers

---
+++ Java/Basics/DataTypes/Numbers/numbers/src/main/java/com/numbers/Main.java
@@ -0,0 +1,28 @@
+package com.numbers;
+
+public class Main {
+    public static void main(String[] args) {
+        byte myNumb = 100;
+        System.out.println(myNumb);
+
+        short myNums = 5000;
+        System.out.println(myNums);
+
+        int myNumi = 100000;
+        System.out.println(myNumi);
+
+        long myNuml = 15000000000L;
+        System.out.println(myNuml);
+
+        float myNumf = 5.75f;
+        System.out.println(myNumf);
+
+        double myNumd = 19.99d;
+        System.out.println(myNumd);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
