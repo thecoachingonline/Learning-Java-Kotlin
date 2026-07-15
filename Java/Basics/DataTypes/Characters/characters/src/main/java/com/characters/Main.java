5a873c942a55cb6536267197db89c932123cdb7c
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-15 01:59:33 +0000

    Characters

---
+++ Java/Basics/DataTypes/Characters/characters/src/main/java/com/characters/Main.java
@@ -0,0 +1,16 @@
+package com.characters;
+
+public class Main {
+    public static void main(String[] args) {
+        char myGrade = 'B';
+        System.out.println(myGrade);
+
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+    }
+}
