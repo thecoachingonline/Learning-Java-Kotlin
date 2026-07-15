9499e2a31ba54f643d95db3d401addb2164ea74f
Y.Nanthachai <thecoachingonline@gmail.com>
2026-07-15 01:53:44 +0000

    Boolean

---
+++ Java/Basics/DataTypes/Boolean/boolean/src/main/java/com/boolean/Main.java
@@ -0,0 +1,10 @@
+package com.boolean;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+    }
+}
