e20c30d9663352027b4d9bc1364f890dd5199662
Y.Nanthachai <thecoachingonline@gmail.com>
2026-08-03 05:28:18 +0000

    Var Keyword

---
+++ Java/Basics/DataTypes/VarKeyword/varkeyword/src/main/java/com/varkeyword/Main.java
@@ -0,0 +1,21 @@
+package com.varkeyword;
+
+public class Main {
+    public static void main(String[] args) {
+        var x = 5;  // x is an int
+        System.out.println(x);
+
+        var myNum = 5;         // int
+        var myDouble = 9.98;   // double
+        var myChar = 'D';      // char
+        var myBoolean = true;  // boolean
+        var myString = "Hello"; // String
+
+        System.out.println(myNum);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myString);
+
+    }
+}
