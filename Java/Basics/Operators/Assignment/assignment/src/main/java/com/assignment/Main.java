22301d5b43a7a8f93738aba2e7daece7bd52a833
Y.Nanthachai <thecoachingonline@gmail.com>
2026-08-15 07:40:47 +0000

    Assignment

---
+++ Java/Basics/Operators/Assignment/assignment/src/main/java/com/assignment/Main.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        x += 5;
+        System.out.println(x);
+
+        int savings = 100; 
+        savings += 50; // add 50 to savings
+        System.out.println("Total savings: " + savings);
+    }
+}
